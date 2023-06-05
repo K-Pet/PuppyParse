f86334450adf810cddbedbeeb94c50657a695576
Kobe Petrus <kobe.petrus07@gmail.com>
2023-06-05 16:12:14 -0400

    Initial Commit

---
+++ PuppyParse/PuppyParseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuppyParseApp.swift
+//  PuppyParse
+//
+//  Created by Kobe Petrus on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PuppyParseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
